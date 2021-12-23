297463d30217d54674d9e972e9a0fd1cd266058f
julian-q <julianhquevedo@gmail.com>
2021-12-23 15:47:39 -0800

    renamed loss, made plot look cooler

--- test.py
+++
@@ -1,9 +0,0 @@
-s = 'relu'
-
-match s:
-    case 'relu':
-        activation_derivative = relu_derivative
-    case 'sigmoid':
-        activation_derivative = sigmoid_derivative
-    case _:
-        raise Exception('Activation function not supported:', s)
